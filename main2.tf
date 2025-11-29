cb1186a5a7cd44ab8efcd7268ace82f7f8350a1b
rissakshi <sakshi@dekatc.com>
2025-11-29 17:24:19 +0530

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # SECURITY GROUP
 # ================================
 resource "aws_security_group" "my_sg" {
-  name = "my-ec2-sg"
+  name = "my-ec2-sg1"
 
   ingress {
     description = "Allow SSH only from your IP"
