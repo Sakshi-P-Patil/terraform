61b3c0d65becf89a126b1afe6efda6072553697b
rissakshi <sakshi@dekatc.com>
2025-11-29 17:37:48 +0530

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "my_sg" {
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-0ecb62995f68bb549"  # Ubuntu 22.04 (us-east-1)
   instance_type          = "t3.small"
-  key_name               = "jenkins.pem"  # <-- Replace with your existing key pair name
+  key_name               = "jenkins"  # <-- Replace with your existing key pair name
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 
   tags = {
